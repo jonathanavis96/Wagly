bac23fe431a73595f269cd7cb80dc27ef0b937b0
WongBater <grafeeti1@gmail.com>
2026-01-09 00:01:13 +0200

    Updated names on checkout page

--- src/pages/BringWaglyHome.tsx
+++ src/pages/BringWaglyHome.tsx
@@ -950,27 +950,27 @@ export default function BringWaglyHome() {
                 <div className="space-y-4">
                   {[
                     {
-                      name: 'Margaret L.',
+                      name: 'Mary',
                       title: 'So adorable',
                       text: 'Bought this puppy for my grandson and he fell in love straight away 💕 First month he took it everywhere and even wanted it in bed so we had to turn it off at night 😄 Months later he still plays with it all the time. Nice to see him off the tablet too.'
                     },
                     {
-                      name: 'Helen R.',
+                      name: 'Helen Rosburg',
                       title: 'Lovely little toy',
                       text: 'We are really happy with this puppy. Keeps the kids busy for ages and they never seem to get tired of it. Such a good gift idea.'
                     },
                     {
-                      name: 'Susan K.',
+                      name: 'Susan Chan',
                       title: 'So much fun',
                       text: 'Did not expect it to be this interactive. It reacts to touch and sound which makes it feel more real. Every time it moves my toddler laughs so much 😂 Also helping him learn to be gentle which I really like.'
                     },
                     {
-                      name: 'Daniel T.',
+                      name: 'Daniel Tompson',
                       title: 'Worth it',
                       text: 'Got this for my nephew and my sister says he hardly puts it down. Seeing how happy he is makes it totally worth it. Would buy again.'
                     },
                     {
-                      name: 'Patricia W.',
+                      name: 'Patricia',
                       title: 'Really impressed',
                       text: 'Such a nice way to teach kids about caring for animals. Our daughter loves looking after it and you can see her learning while playing. Very happy with this purchase.'
                     }
