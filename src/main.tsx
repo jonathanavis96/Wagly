7ad7527117ac3ab6517a518f0e5ac32742e6d9c8
WongBater <grafeeti1@gmail.com>
2026-01-08 18:58:36 +0200

    Reset scroll position on route change

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BringWaglyHome from "./pages/BringWaglyHome";
+import ScrollToTop from "./components/ScrollToTop";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/bring-wagly-home" element={<BringWaglyHome />} />
