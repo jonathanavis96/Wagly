ae96c55ec5fa89ebc67540e7a73853abf4c2126d
jonathanavis96 <jonathanavis96@users.noreply.github.com>
2026-01-11 16:59:56 +0000

    Deploying to gh-pages from @ jonathanavis96/Wagly@7bbbbf8aa9eb79037ea5afdea9c731b1379840d9 🚀

---
+++ pr-1/og-meta-updater.js
@@ -0,0 +1,21 @@
+﻿// Script to dynamically set OG image URLs based on current location
+(function() {
+  const baseUrl = window.location.origin + window.location.pathname.replace(/\/[^\/]*$/, '/');
+  const ogImageUrl = baseUrl + 'og_image.png';
+  
+  // Update OG image
+  const ogImage = document.querySelector('meta[property="og:image"]');
+  if (ogImage) ogImage.setAttribute('content', ogImageUrl);
+  
+  // Update Twitter image
+  const twitterImage = document.querySelector('meta[name="twitter:image"]');
+  if (twitterImage) twitterImage.setAttribute('content', ogImageUrl);
+  
+  // Update OG URL
+  const ogUrl = document.querySelector('meta[property="og:url"]');
+  if (ogUrl) ogUrl.setAttribute('content', window.location.href.split('?')[0]);
+  
+  // Update Twitter URL
+  const twitterUrl = document.querySelector('meta[name="twitter:url"]');
+  if (twitterUrl) twitterUrl.setAttribute('content', window.location.href.split('?')[0]);
+})();
