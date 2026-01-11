48f7b39d3310d76add1f2e11c08bf96d250bf235
jonathanavis96 <jonathanavis96@users.noreply.github.com>
2026-01-11 17:02:58 +0000

    Deploying to gh-pages from @ jonathanavis96/Wagly@f7ad6933bb7547787135901af00633ab6fe413fa 🚀

--- pr-1/og-meta-updater.js
+++ pr-1/og-meta-updater.js
@@ -1,7 +1,24 @@
 ﻿// Script to dynamically set OG image URLs based on current location
 (function() {
-  const baseUrl = window.location.origin + window.location.pathname.replace(/\/[^\/]*$/, '/');
+  // Get the base path from the current page URL
+  // For /Wagly/pr-1/index.html -> /Wagly/pr-1/
+  // For /Wagly/index.html -> /Wagly/
+  // For /Wagly/ -> /Wagly/
+  let pathname = window.location.pathname;
+  
+  // If pathname ends with index.html, remove it
+  if (pathname.endsWith('index.html')) {
+    pathname = pathname.slice(0, -10); // Remove 'index.html'
+  }
+  
+  // Ensure trailing slash
+  if (!pathname.endsWith('/')) {
+    pathname = pathname + '/';
+  }
+  
+  const baseUrl = window.location.origin + pathname;
   const ogImageUrl = baseUrl + 'og_image.png';
+  const pageUrl = window.location.href.split('?')[0].split('#')[0];
   
   // Update OG image
   const ogImage = document.querySelector('meta[property="og:image"]');
@@ -13,9 +30,16 @@
   
   // Update OG URL
   const ogUrl = document.querySelector('meta[property="og:url"]');
-  if (ogUrl) ogUrl.setAttribute('content', window.location.href.split('?')[0]);
+  if (ogUrl) ogUrl.setAttribute('content', pageUrl);
   
   // Update Twitter URL
   const twitterUrl = document.querySelector('meta[name="twitter:url"]');
-  if (twitterUrl) twitterUrl.setAttribute('content', window.location.href.split('?')[0]);
+  if (twitterUrl) twitterUrl.setAttribute('content', pageUrl);
+  
+  console.log('OG Meta Updater:', {
+    pathname: pathname,
+    baseUrl: baseUrl,
+    ogImageUrl: ogImageUrl,
+    pageUrl: pageUrl
+  });
 })();
